14a2ad0d7188f4a3b605230bddf8a707f92af0e0
Dinh Quy Pham <89065456+danielpham29@users.noreply.github.com>
2021-08-28 20:55:52 +0700

    Add files via upload

---
+++ Tam-Giac-Vuong-Can-Nguoc.py
@@ -0,0 +1,24 @@
+print("Dinh Quy Pham")
+a = "Một bài toán hay về tam giác vuông cân góc phải dưới"
+print(a.center(25))
+print("-"*35)
+nhap_cao = None
+while True :
+    try:
+        nhap_cao = int(input("Nhập vào chiều cao tam giác : "))
+        for a in range(nhap_cao):
+            for b in range(nhap_cao):
+                # Chỗ này là bài toán hơi ngược, vẽ ra giấy sẽ thấy
+                if b < (nhap_cao - 1) - a:
+                    print("   ",end="")
+                else:
+                    print(" * ",end="")
+            print()
+        # Cần lệnh break để thoát chương trình, không cần nhập lại dữ liệu
+        break
+    except ValueError:
+        print("Má nó nhập số thôi cũng sai hả thằng đầu buồi ?")
+        continue
+
+
+
